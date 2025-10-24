2afa5f85fac1705e9f5177ea7e41e63d604060c6
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-10-24 15:49:26 +0200

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,3 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/Roblox-ESP-Library/refs/heads/main/Source/Main.lua"))();
+Library:Initiate();
+-- this is barely even a library yet, its not finished
