7f926e4572888e9edc74fa67eb02703b984cd07b
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-11-25 22:37:31 +0100

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,3 +1,10 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/Roblox-ESP-Library/refs/heads/main/Source/Main.lua"))();
+Library.Settings.Box.Enabled = true;
+Library.Settings.Name.Enabled = true;
+Library.Settings.Healthbar.Enabled = true;
+
+Library.Settings.Box.Colors[1] = Color3.new(1, 0, 0);
+Library.Settings.Name.Colors[1] = Color3.new(1, 0, 0);
+Library.Settings.Healthbar.Colors[1] = Color3.new(0, 1, 0);
+
 Library:Initiate();
--- this is barely even a library yet, its not finished
